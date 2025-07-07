b37f7bcd3f86498515061411e760fd902f8f9bf9
Zayed Ahmed <zayed.ah06@gmail.com>
2025-07-07 19:20:17 +0600

    full pytest code for book_cr resouce in books.py

--- tests/test_books_cr.py
+++ tests/test_books_cr.py
@@ -30,9 +30,65 @@ def token():
             "refresh_token": json_data["refresh_token"]
         }
 
+def test_books_pagination(client, token):
+    access_token = token["access_token"]
+    response = client.get('/api/v1/books?page=2&per_page=5',
+    headers={
+        "Authorization": f"Bearer {access_token}"
+    })
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert "page" in str(json_data).lower()
+    assert "per_page" in str(json_data).lower()
+    assert "total_items" in str(json_data).lower()
+    assert "total_pages" in str(json_data).lower()
+
+def test_books_search_title(client, token):
+    access_token = token["access_token"]
+    response = client.get('/api/v1/books?title=Dune',
+    headers={
+        "Authorization": f"Bearer {access_token}"
+    })
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert "frank herbert" in str(json_data).lower()
+    assert "page" in str(json_data).lower()
+    assert "per_page" in str(json_data).lower()
+    assert "total_items" in str(json_data).lower()
+    assert "total_pages" in str(json_data).lower()
+
+def test_books_search_author(client, token):
+    access_token = token["access_token"]
+    response = client.get('/api/v1/books?author=George Orwell',
+    headers={
+        "Authorization": f"Bearer {access_token}"
+    })
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert "1984" in str(json_data).lower()
+    assert "page" in str(json_data).lower()
+    assert "per_page" in str(json_data).lower()
+    assert "total_items" in str(json_data).lower()
+    assert "total_pages" in str(json_data).lower()
+
+def test_search_author_and_title(client, token):
+    access_token = token["access_token"]
+    response = client.get('/api/v1/books?title=The Alchemist&author=Paulo Coelho',
+    headers={
+        "Authorization": f"Bearer {access_token}"
+    })
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert "the alchemist" and "paulo coelho" in str(json_data).lower()
+    assert "page" in str(json_data).lower()
+    assert "per_page" in str(json_data).lower()
+    assert "total_items" in str(json_data).lower()
+    assert "total_pages" in str(json_data).lower()
+
 def test_adding_books(client, token):
     access_token = token["access_token"]
-
+    
+    # currently books is empty but its used to post books in bulk
     books = [
   {
     "title": "One Hundred Years of Solitude",
@@ -85,6 +141,7 @@ def test_adding_books(client, token):
     "genre": "Mystery"
   }
 ]
+   
     for book in books:
         response = client.post('/api/v1/books', json={
         "title": book["title"],
